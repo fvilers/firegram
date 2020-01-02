da76f65ca576989251cecf1c736fe855417c2660
Fabian Vilers <fabian.vilers@dev-one.com>
2020-01-02 19:03:22 +0100

    Add the SignUpPage component

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const HomePage: React.FC = () => {
   return (
     <div>
       <h1>Firegram</h1>
+      <p>
+        <Link to="/sign-up">Sign up</Link>
+      </p>
     </div>
   );
 };
---
+++ src/pages/SignUpPage.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SignUpPage: React.FC = () => {
+  return (
+    <div>
+      <h1>Sign up</h1>
+    </div>
+  );
+};
+
+export default SignUpPage;
