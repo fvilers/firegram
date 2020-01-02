c24f54f2f710815adcfeb06c51b4350083ddf3ac
Fabian Vilers <fabian.vilers@dev-one.com>
2020-01-02 20:08:22 +0100

    Hide password from redux devtools

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,11 +1,22 @@
-import { applyMiddleware, createStore } from "redux";
+import { applyMiddleware, createStore, AnyAction } from "redux";
 import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
+import { SIGN_UP_STARTED } from "./actions/sign-up";
+import { merge } from "./merge";
 import rootReducer from "./reducers";
 
+const actionSanitizer = <A extends AnyAction>(action: A): A => {
+  switch (action.type) {
+    case SIGN_UP_STARTED:
+      return merge(action, { payload: { password: undefined } });
+    default:
+      return action;
+  }
+};
+const composeEnhancers = composeWithDevTools({ actionSanitizer });
 const store = createStore(
   rootReducer,
-  composeWithDevTools(applyMiddleware(thunk))
+  composeEnhancers(applyMiddleware(thunk))
 );
 
 export default store;
