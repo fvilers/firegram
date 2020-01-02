effdf0e29fb52f2dd2640bb7a20c8a4a85f34b8e
Fabian Vilers <fabian.vilers@dev-one.com>
2020-01-02 20:19:21 +0100

    Add sign in implementation using redux

--- src/containers/SignIn.tsx
+++ src/containers/SignIn.tsx
@@ -1,8 +1,27 @@
 import React from "react";
-import SignInForm from "../components/SignInForm";
+import { useLocation } from "react-router";
+import { useSelector, useDispatch } from "react-redux";
+import { AppState, AsyncOperation } from "../redux/state";
+import { signIn } from "../redux/actions/sign-in";
+import SignInForm, { SignInFormValues } from "../components/SignInForm";
 
 const SignIn: React.FC = () => {
-  return <SignInForm onSubmit={values => console.log(values)} />;
+  const location = useLocation();
+  const { busy, errorMessage } = useSelector<AppState, AsyncOperation>(
+    s => s.auth.ui.signIn
+  );
+  const dispatch = useDispatch();
+  const handleSubmit = ({ email, password }: SignInFormValues) => {
+    dispatch(signIn(email, password, location?.state?.from?.pathname));
+  };
+
+  return (
+    <SignInForm
+      disabled={busy}
+      errorMessage={errorMessage}
+      onSubmit={handleSubmit}
+    />
+  );
 };
 
 export default SignIn;
