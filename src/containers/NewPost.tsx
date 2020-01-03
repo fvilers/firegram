7fd8f6b9c9f4ae1b8ed99e99e46e2d9f799d2c80
Fabian Vilers <fabian.vilers@dev-one.com>
2020-01-03 09:58:51 +0100

    Add new post implementation in redux

--- src/containers/NewPost.tsx
+++ src/containers/NewPost.tsx
@@ -1,12 +1,29 @@
 import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { AppState, AsyncOperation } from "../redux/state";
+import { createPost } from "../redux/actions/create-post";
 import NewPostForm, { NewPostFormValues } from "../components/NewPostForm";
 
 const NewPost: React.FC = () => {
-  const handleSubmit = (values: NewPostFormValues) => {
-    console.log(values);
+  const { busy, errorMessage } = useSelector<AppState, AsyncOperation>(
+    s => s.post.ui.createPost
+  );
+  const dispatch = useDispatch();
+  const handleSubmit = ({
+    caption,
+    fileContent,
+    fileName
+  }: NewPostFormValues) => {
+    dispatch(createPost(caption, fileContent, fileName));
   };
 
-  return <NewPostForm onSubmit={handleSubmit} />;
+  return (
+    <NewPostForm
+      disabled={busy}
+      errorMessage={errorMessage}
+      onSubmit={handleSubmit}
+    />
+  );
 };
 
 export default NewPost;
