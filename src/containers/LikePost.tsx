5e9cb11b91069fd84c0840897125d5210e7470d7
Fabian Vilers <fabian.vilers@dev-one.com>
2020-01-13 11:47:05 +0100

    Handle like or unlike depending on current likes

--- src/containers/LikePost.tsx
+++ src/containers/LikePost.tsx
@@ -3,6 +3,7 @@ import { useSelector, useDispatch } from "react-redux";
 import { Button, Popup } from "semantic-ui-react";
 import { AppState, AsyncOperation } from "../redux/state";
 import { likePost } from "../redux/actions/like-post";
+import { unlikePost } from "../redux/actions/unlike-post";
 
 type Props = {
   id: string;
@@ -12,17 +13,27 @@ const LikePost: React.FC<Props> = ({ id }) => {
   const currentUser = useSelector<AppState, firebase.User | null>(
     s => s.auth.currentUser
   );
-  const { busy, errorMessage } = useSelector<AppState, AsyncOperation>(
+  const hasLiked = useSelector<AppState, boolean>(s =>
+    currentUser ? s.post.collection[id]?.likes[currentUser?.uid] : false
+  );
+  const likeAction = useSelector<AppState, AsyncOperation>(
     s => s.post.ui.likePost
+  );
+  const unlikeAction = useSelector<AppState, AsyncOperation>(
+    s => s.post.ui.unlikePost
   );
   const dispatch = useDispatch();
   const handleClick = () => {
-    dispatch(likePost(id));
+    dispatch(!hasLiked ? likePost(id) : unlikePost(id));
   };
 
   if (!currentUser) {
     return null;
   }
+
+  const busy = likeAction.busy || unlikeAction.busy;
+  const errorMessage = likeAction.errorMessage || unlikeAction.errorMessage;
+  const color = hasLiked ? "red" : undefined;
 
   return (
     <Popup
@@ -31,6 +42,7 @@ const LikePost: React.FC<Props> = ({ id }) => {
       trigger={
         <Button
           basic
+          color={color}
           disabled={busy}
           icon="like"
           loading={busy}
